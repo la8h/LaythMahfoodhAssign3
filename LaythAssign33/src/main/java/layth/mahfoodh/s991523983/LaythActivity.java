6d5b94ae9eee3fd23bc0ef89083fb0f602aa6a82
mahfoodh1 <mahfoodh@sheridancollege.ca>
2021-03-23 22:08:02 -0400

    Create activity and three fragments

---
+++ LaythAssign33/src/main/java/layth/mahfoodh/s991523983/LaythActivity.java
@@ -0,0 +1,17 @@
+/*
+Layth Mahfoodh s991523983 PROG38448
+ */
+package layth.mahfoodh.s991523983;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LaythActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
